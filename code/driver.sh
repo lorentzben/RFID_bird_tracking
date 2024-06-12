4eadbbe261ebeb900f36f4f076b1dc21ab7bb18c
Benjamin Lorentz <bjl34716@uga.edu>
2024-06-12 15:25:53 -0400

    add all analysis back in

--- code/driver.sh
+++ code/driver.sh
@@ -57,20 +57,20 @@ fi
 
 singularity run docker://lorentzb/rfid:2.1 Rscript room_2_analysis.r > ../logs/"$TS"_rm2.log
 
-# # Run Room 3 Individual Analysis 
+# Run Room 3 Individual Analysis 
 
-# singularity run docker://lorentzb/rfid:2.1 Rscript room_3_analysis.r > ../logs/"$TS"_rm3.log
+singularity run docker://lorentzb/rfid:2.1 Rscript room_3_analysis.r > ../logs/"$TS"_rm3.log
 
-# # Run Room 8 Individual Analysis
+# Run Room 8 Individual Analysis
 
-# singularity run docker://lorentzb/rfid:2.1 Rscript room_8_analysis.r > ../logs/"$TS"_rm8.log
+singularity run docker://lorentzb/rfid:2.1 Rscript room_8_analysis.r > ../logs/"$TS"_rm8.log
 
-# # Run Room 11 Individual Analysis
+# Run Room 11 Individual Analysis
 
-# singularity run docker://lorentzb/rfid:2.1 Rscript room_11_analysis.r > ../logs/"$TS"_rm11.log
+singularity run docker://lorentzb/rfid:2.1 Rscript room_11_analysis.r > ../logs/"$TS"_rm11.log
 
-# # Run All Room Joint Analysis
+# Run All Room Joint Analysis
 
-# singularity run docker://lorentzb/rfid:2.1 Rscript joint_analysis.r > ../logs/"$TS"_joint.log
+singularity run docker://lorentzb/rfid:2.1 Rscript joint_analysis.r > ../logs/"$TS"_joint.log
 
 echo "Script finished"
