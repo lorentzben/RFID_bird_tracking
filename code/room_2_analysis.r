302c1bfb719972de983364c1a15f4bfc456f70a8
Benjamin Lorentz <bjl34716@uga.edu>
2024-06-12 11:15:27 -0400

    change X1,2,3 to bottom middle top

--- code/room_2_analysis.r
+++ code/room_2_analysis.r
@@ -119,7 +119,7 @@ room_2_all_room_time_budget <- room_2_interval |>
 print(room_2_all_room_time_budget)
 
 room_2_all_room_time_budget |>
- select(c(tagname, Interval.1., Interval.2., X1, X2, X3)) |> 
+ select(c(tagname, Interval.1., Interval.2., bottom, middle, top)) |> 
  write.csv(row.names=F, '../output/all_rooms/room_2_all_room_time_budget.csv')
 
 room_2_interval |> 
